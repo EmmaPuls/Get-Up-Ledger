f2a8e7d84064ff63036b2bc28ba32a87230e94d9
Emma Puls <got2love@gmail.com>
2025-02-22 13:57:45 +0800

    Initial Commit

---
+++ Get Up AppTests/Get_Up_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Get_Up_AppTests.swift
+//  Get Up AppTests
+//
+//  Created by Emma Puls on 22/2/2025.
+//
+
+import Testing
+@testable import Get_Up_App
+
+struct Get_Up_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Get Up AppUITests/Get_Up_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Get_Up_AppUITestsLaunchTests.swift
+//  Get Up AppUITests
+//
+//  Created by Emma Puls on 22/2/2025.
+//
+
+import XCTest
+
+final class Get_Up_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
